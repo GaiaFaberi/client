b9859ffdf23b41c280e7510bf1f2811c2f94f779
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 09:57:10 +0200

    aggiunta del menu

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -10,30 +10,85 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
+        
         System.out.println("client partito");
         Socket s = new Socket("localhost", 3000);
-        System.out.println("il client si è collegato");
-         
+
+        System.out.println("il client si è collegato \n");
+
+        String scelta;
 
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
         
         String stringaInviata;
 
-        do{
-            
-            Scanner scanner = new Scanner(System.in);
-            stringaInviata = scanner.nextLine();
-            out.writeBytes(stringaInviata + "\n");
-            String stringaRicevuta = in.readLine();
+        Scanner scanner = new Scanner(System.in);
 
-            if(!stringaInviata.equals("!")){
-               
-                System.out.println(stringaRicevuta); 
+        do{
+            System.out.println("1- Trasformare stringa in maiuscolo \n");
+            System.out.println("2- Trasformare stringa in minuscolo \n");
+            System.out.println("3- Ribaltare i caratteri della stringa \n");
+            System.out.println("4- Contare il numero di caratteri \n");
+            System.out.println("0- Chiudere la connessione \n");
+
+            scelta = scanner.nextLine();
+
+
+            switch(scelta){
+
+                case "1": 
+
+                    System.out.println("Digita la frase: ");
+                    stringaInviata = scanner.nextLine();
+                    out.writeBytes("U" + "\n");
+                    if(in.readLine().equals("frase?")){
+                        out.writeBytes(stringaInviata + "\n");
+                        String stringaRicevuta1 = in.readLine();
+                        System.out.println(stringaRicevuta1 + "\n");
+
+                    }
+                
+                    case "2": 
+
+                        System.out.println("Digita la frase: ");
+                        stringaInviata = scanner.nextLine();
+                        out.writeBytes("L" + "\n");
+                        if(in.readLine().equals("frase?")){
+                            out.writeBytes(stringaInviata + "\n");
+                            String stringaRicevuta1 = in.readLine();
+                            System.out.println(stringaRicevuta1 + "\n");
+
+                        }
+                        break;
+                    
+                        case "3": 
+
+                            System.out.println("Digita la frase: ");
+                            stringaInviata = scanner.nextLine();
+                            out.writeBytes("R" + "\n");
+                            if(in.readLine().equals("frase?")){
+                                out.writeBytes(stringaInviata + "\n");
+                                String stringaRicevuta1 = in.readLine();
+                                System.out.println(stringaRicevuta1 + "\n");
+
+                            }
+                            break;
+                        
+                        case "4":
+                            System.out.println("Digita la frase: ");
+                            stringaInviata = scanner.nextLine();
+                            out.writeBytes("C" + "\n");
+                            if(in.readLine().equals("frase?")){
+                                out.writeBytes(stringaInviata + "\n");
+                                String stringaRicevuta1 = in.readLine();
+                                System.out.println(stringaRicevuta1 + "\n");
+
+                            }
+                        break;   
             }
-            
-            
-        }while(!stringaInviata.equals("!"));
+
+        }while(!scelta.equals("0"));
         s.close();
     }
 }
